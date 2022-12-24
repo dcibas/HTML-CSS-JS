63913f1a825e24f48ee343526028fccbb87cd3f0
dcibas <dcibas.art@gmail.com>
2022-12-24 16:27:59 +0200

    Front-End files ongoing project

--- VoteApp/Post Vote Posted Form (Voter View)/PostVoting-PostedVoteForm.js
+++ VoteApp/Post Vote Posted Form (Voter View)/PostVoting-PostedVoteForm.js
@@ -23,46 +23,74 @@ function logout(e) {
   window.location.href = "http://127.0.0.1:5500/Login%20Form/VoteApp-Login.html";
 }
 
-let votingchoice1Btn = document.getElementById("votingchoice1")
-votingchoice1Btn.addEventListener("click", votingchoice1);
+// let votingchoice1Btn = document.getElementById("votingchoice1")
+// votingchoice1Btn.addEventListener("click", votingchoice1);
 
-function votingchoice1 (e) {
-e.preventDefault();
-document.getElementById("votingchoice1").style.background = 'rgb(242, 255, 0)';
-document.getElementById("votingchoice2").style.background = 'white';
-document.getElementById("votingchoice3").style.background = 'white';
-document.getElementById("votingchoice4").style.background = 'white';
-}
+// function votingchoice1 (e) {
+// e.preventDefault();
+// document.getElementById("votingchoice1").style.background = 'rgb(242, 255, 0)';
+// document.getElementById("votingchoice2").style.background = 'white';
+// document.getElementById("votingchoice3").style.background = 'white';
+// document.getElementById("votingchoice4").style.background = 'white';
+// }
 
-let votingchoice2Btn = document.getElementById("votingchoice2")
-votingchoice2Btn.addEventListener("click", votingchoice2);
+// let votingchoice2Btn = document.getElementById("votingchoice2")
+// votingchoice2Btn.addEventListener("click", votingchoice2);
 
-function votingchoice2 (e) {
-e.preventDefault();
-document.getElementById("votingchoice2").style.background = 'rgb(242, 255, 0)';
-document.getElementById("votingchoice1").style.background = 'white';
-document.getElementById("votingchoice3").style.background = 'white';
-document.getElementById("votingchoice4").style.background = 'white';
-}
+// function votingchoice2 (e) {
+// e.preventDefault();
+// document.getElementById("votingchoice2").style.background = 'rgb(242, 255, 0)';
+// document.getElementById("votingchoice1").style.background = 'white';
+// document.getElementById("votingchoice3").style.background = 'white';
+// document.getElementById("votingchoice4").style.background = 'white';
+// }
 
-let votingchoice3Btn = document.getElementById("votingchoice3")
-votingchoice3Btn.addEventListener("click", votingchoice3);
+// let votingchoice3Btn = document.getElementById("votingchoice3")
+// votingchoice3Btn.addEventListener("click", votingchoice3);
 
-function votingchoice3 (e) {
-e.preventDefault();
-document.getElementById("votingchoice3").style.background = 'rgb(242, 255, 0)';
-document.getElementById("votingchoice4").style.background = 'white';
-document.getElementById("votingchoice2").style.background = 'white';
-document.getElementById("votingchoice1").style.background = 'white';
-}
+// function votingchoice3 (e) {
+// e.preventDefault();
+// document.getElementById("votingchoice3").style.background = 'rgb(242, 255, 0)';
+// document.getElementById("votingchoice4").style.background = 'white';
+// document.getElementById("votingchoice2").style.background = 'white';
+// document.getElementById("votingchoice1").style.background = 'white';
+// }
 
-let votingchoice4Btn = document.getElementById("votingchoice4")
-votingchoice4Btn.addEventListener("click", votingchoice4);
+// let votingchoice4Btn = document.getElementById("votingchoice4")
+// votingchoice4Btn.addEventListener("click", votingchoice4);
 
-function votingchoice4 (e) {
-e.preventDefault();
-document.getElementById("votingchoice4").style.background = 'rgb(242, 255, 0)';
-document.getElementById("votingchoice3").style.background = 'white';
-document.getElementById("votingchoice2").style.background = 'white';
-document.getElementById("votingchoice1").style.background = 'white';
+// function votingchoice4 (e) {
+// e.preventDefault();
+// document.getElementById("votingchoice4").style.background = 'rgb(242, 255, 0)';
+// document.getElementById("votingchoice3").style.background = 'white';
+// document.getElementById("votingchoice2").style.background = 'white';
+// document.getElementById("votingchoice1").style.background = 'white';
+// }
+
+let voteBtn = document.getElementById("vote");
+voteBtn.addEventListener("click", vote);
+
+function vote(e) {
+  e.preventDefault();
+
+  let myVotinchoice = document.getElementById("votingchoices").value;
+  // let myVotinchoice = document.querySelector('input');
+  // let myVotinchoice = document.getElementsByName("votingchoice").value;
+  // let myVotinchoice = document.querySelectorAll('input[type=button]');
+ 
+  fetch(`http://localhost:8090/api/votepost/vote`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Authorization": `Bearer ` + localStorage.getItem("token")
+    },
+    body: JSON.stringify({
+      votingChoice: myVotinchoice,
+      votePostId: localStorage.getItem("votepostid"),
+      userId: localStorage.getItem("userid"),
+    }),
+  })
+    .then((res) => res.json())
+    .then(alert("Congratulations! You have successfully submited your vote!"))
+    .then(location.reload());
 }
