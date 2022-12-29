937a39c9acfa3b8cc06d34572edc938ef3a6f034
dcibas <dcibas.art@gmail.com>
2022-12-30 00:06:53 +0200

    Front-End files ongoing project

--- VoteApp/Post Vote Results (User View)/PostVoting-Results.js
+++ VoteApp/Post Vote Results (User View)/PostVoting-Results.js
@@ -1,26 +1,71 @@
-let results = window.localStorage.getItem("results");
-let resultsObj = JSON.parse(results);
-let votepost = window.localStorage.getItem("votepost");
-let votepostObj = JSON.parse(votepost);
-let resultsTitle = JSON.parse(results).myTitle;
-let resultsVotingChoice1Points = JSON.parse(results).myvotingChoice1Points;
-let resultsVotingChoice2Points = JSON.parse(results).myvotingChoice2Points;
-let resultsVotingChoice3Points = JSON.parse(results).myvotingChoice3Points;
-let resultsVotingChoice4Points = JSON.parse(results).myvotingChoice4Points;
-let votepostEndDate = JSON.parse(votepost).myEnddate;
 
-document.getElementById("resultsTitle").textContent +=
-  " " + resultsTitle;
-document.getElementById("votingEndDate").textContent +=
-  " " + votepostEndDate;
-document.getElementById("votingchoice1Points").textContent +=
-  " " + resultsVotingChoice1Points;
-document.getElementById("votingchoice2Points").textContent +=
-  " " + resultsVotingChoice2Points;
-document.getElementById("votingchoice3Points").textContent +=
-  " " + resultsVotingChoice3Points;
-document.getElementById("votingchoice4Points").textContent +=
-  " " + resultsVotingChoice4Points;
+let myresultsId = localStorage.getItem("resultsid");
+
+fetch(`http://localhost:8090/api/results/${myresultsId}`, {method: "GET",
+  headers: {
+    "Content-type": "application/json",
+    "Authorization": `Bearer ` + localStorage.getItem("token")
+  },})
+    .then((res) => res.json())
+    .then((data) => {
+        document.getElementById(
+          "votingchoice1Points"
+        ).innerHTML += `<br> Completely Against:
+      ${data.votingPoints1} <br><br> Partially Against: ${data.votingPoints2} 
+      <br><br> Partially Agree: ${data.votingPoints3} 
+      <br><br> Completely Agree: ${data.votingPoints4}
+      <br><br><br>
+      `;
+      
+    });
+
+// fetch(`http://localhost:8090/api/results`, {method: "GET",
+//   headers: {
+//     "Content-type": "application/json",
+//     "Authorization": `Bearer ` + localStorage.getItem("token")
+//   },})
+//     .then((res) => res.json())
+//     .then((data) => {
+//       document.getElementById("votingchoice1Points").innerHTML = "";
+//       for (let i = 0; i < data.length; i++) {
+//         document.getElementById(
+//           "votingchoice1Points"
+//         ).innerHTML += `<br> Results id: ${data[i].id} <br> Voting Choice 1 Points:
+//       ${data[i].votingPoints1} <br> Voting Choice 2 Points: ${data[i].votingPoints2} 
+//       <br> Voting Choice 3 Points: ${data[i].votingPoints3} 
+//       <br> Voting Choice 4 Points: ${data[i].votingPoints4}
+//       <br><br><br>
+//       `;
+//       }
+//     });
+
+// let results = window.localStorage.getItem("results");
+// let resultsObj = JSON.parse(results);
+
+ let votepost = window.localStorage.getItem("votepost");
+ let votepostObj = JSON.parse(votepost);
+ let resultsTitle = JSON.parse(votepost).myTitle;
+
+// let resultsTitle = JSON.parse(results).myTitle;
+// let resultsVotingChoice1Points = JSON.parse(results).myvotingChoice1Points;
+// let resultsVotingChoice2Points = JSON.parse(results).myvotingChoice2Points;
+// let resultsVotingChoice3Points = JSON.parse(results).myvotingChoice3Points;
+// let resultsVotingChoice4Points = JSON.parse(results).myvotingChoice4Points;
+ let votepostEndDate = JSON.parse(votepost).myEnddate;
+
+ document.getElementById("resultsTitle").textContent +=
+   " " + resultsTitle;
+ document.getElementById("votingEndDate").textContent +=
+   " " + votepostEndDate;
+
+// document.getElementById("votingchoice1Points").textContent +=
+//   " " + resultsVotingChoice1Points;
+// document.getElementById("votingchoice2Points").textContent +=
+//   " " + resultsVotingChoice2Points;
+// document.getElementById("votingchoice3Points").textContent +=
+//   " " + resultsVotingChoice3Points;
+// document.getElementById("votingchoice4Points").textContent +=
+//   " " + resultsVotingChoice4Points;
 
 let backBtn = document.getElementById("back");
 backBtn.addEventListener("click", back);
